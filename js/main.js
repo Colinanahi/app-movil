fb610830e52b4df92573b331c5f6c279b1622886
Anahi14 <62029123+Colinanahi@users.noreply.github.com>
2021-11-17 14:38:41 -0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,17 @@
+
+
+$(document).ready(function(){
+    var sonido = document.getElementById("sonido");
+    var sonido1 = document.getElementById("sonido1");
+    var sonido2 = document.getElementById("sonido2");
+    var sonido3 = document.getElementById("sonido3");
+    var sonido4 = document.getElementById("sonido4");
+    var sonido5 = document.getElementById("sonido5");
+    var sonido6 = document.getElementById("sonido6");
+    var sonido7 = document.getElementById("sonido7");
+    var sonido8 = document.getElementById("sonido8");
+    var sonido9 = document.getElementById("sonido6");
+    var sonido10 = document.getElementById("sonido6");
+    
+
+});
